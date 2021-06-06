4b95fae53364c59da51b7c0b7bb2a619839c9bf0
whong123 <1239918017@qq.com>
2021-06-06 10:15:18 +0800

    更新2

--- src/main/java/com/example/mydemo/unitl/beanunitl/ZdactioninfoUnitl.java
+++ src/main/java/com/example/mydemo/unitl/beanunitl/ZdactioninfoUnitl.java
@@ -8,12 +8,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ZdactioninfoUnitl {
-    public static final int zdtype [] = {0,1};//类型：0-自己，-1敌人
     public static final int percentage [] = {0,1};//百分比：0不是，1是
-    public static final int typeaction [] = {0,1,2,3,4,5};//0开始时累增-1自身效果-2开始-3结束时-4命中-5被命中
+    public static final int typeaction [] = {0,1,2,3,4};//0开始时累增-1开始-2结束时-3命中
     public static final String typevalue [] = {"hp","mp","gjl","fyl","jueduifyl","jueduigjl"};//作用的属性
 
-    public static Zdactioninfo getHpActionInfo_hp(Integer zdactionid ){
+    public static Zdactioninfo getHpActionInfo_hp(Integer zdactionid){
         Zdactioninfo zdactioninfo = new Zdactioninfo(zdactionid);
         zdactioninfo.setTypevalue("hp");
         zdactioninfo.setPercentage(MyUuid.getRandomInt(2)-1);
@@ -39,36 +38,25 @@ public class ZdactioninfoUnitl {
         Map<String,Object> map = new HashMap<>();
         long o = 0l;
         long max = 0l;
-        map.put("typeaction",MyUuid.getRandomInt(5));
+        map.put("typeaction",MyUuid.getRandomInt(3) - 1);
         int swt = (int) map.get("typeaction");
         switch (swt){
-            case 1://无敌状态
-                map.put("onevalue",o);
-                map.put("twovalue",o);
-                map.put("threevalue",o);
-                break;
-            case 2|3://开始、结束时
+            case 1://开始、结束时
+            case 2://开始、结束时
                  o = MyUuid.getRandom(14)+9;
                  max = o + (MyUuid.getRandom(15)-1);
                 map.put("onevalue",o);
                 map.put("twovalue",max);
                 map.put("threevalue",max);
                 break;
-            case 4://命中敌人时
+            case 3://命中敌人时
                  o = MyUuid.getRandom(14)+4;
                  max = o + (MyUuid.getRandom(10)-1);
                 map.put("onevalue",o);
                 map.put("twovalue",max);
                 map.put("threevalue",max);
                 break;
-            case 5://被敌人命中
-                 o = MyUuid.getRandom(15)+12;
-                 max = o + (MyUuid.getRandom(15)-1);
-                map.put("onevalue",o);
-                map.put("twovalue",max);
-                map.put("threevalue",max);
-                break;
-            default://
+            default://开始时累加
                  o = MyUuid.getRandom(4)+3;
                  max = o + (MyUuid.getRandom(3)-1);
                 map.put("onevalue",o);
@@ -81,35 +69,29 @@ public class ZdactioninfoUnitl {
 
     public static Map getOntTwoThreeValue_1(){
         Map<String,Object> map = new HashMap<>();
-        long o = 0l;
-        long max = 0l;
-        map.put("typeaction",MyUuid.getRandomInt(5));
+        long o = 1l;
+        long max = 1l;
+        map.put("typeaction",MyUuid.getRandomInt(3) - 1);
         int swt = (int) map.get("typeaction");
         switch (swt){
-            case 1://去除状态1~3个
-                o = MyUuid.getRandom(1);
-                max = o + (MyUuid.getRandom(3)-1);
+            case 1://开始、结束时
+            case 2://开始、结束时
+                o = MyUuid.getRandom(50)+24;
+                max = o + (MyUuid.getRandom(15)-1);
                 map.put("onevalue",o);
                 map.put("twovalue",max);
                 map.put("threevalue",max);
                 break;
-            case 2|3://开始、结束时
-                o = MyUuid.getRandom(9)+9;
-                max = o + (MyUuid.getRandom(10)-1);
+            case 3://命中敌人时
+                o = MyUuid.getRandom(75)+44;
+                max = o + (MyUuid.getRandom(25)-1);
                 map.put("onevalue",o);
                 map.put("twovalue",max);
                 map.put("threevalue",max);
                 break;
-            case 4|5://命中敌人时 | 被敌人命中
-                o = MyUuid.getRandom(14)+4;
-                max = o + (MyUuid.getRandom(10)-1);
-                map.put("onevalue",o);
-                map.put("twovalue",max);
-                map.put("threevalue",max);
-                break;
-            default://
-                o = MyUuid.getRandom(4)+3;
-                max = o + (MyUuid.getRandom(3)-1);
+            default://开始时累加
+                o = MyUuid.getRandom(34)+3;
+                max = o + (MyUuid.getRandom(19)-1);
                 map.put("onevalue",o);
                 map.put("twovalue",max);
                 map.put("threevalue",max);
